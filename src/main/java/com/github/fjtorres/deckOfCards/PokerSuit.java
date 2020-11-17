a366fcf4fe678a0f5ecad39640807e42fd99e3a4
Francisco Javier Torres <fjtorres@zerocopy.be>
2020-11-17 19:18:33 +0100

    Add symbol field to poker suit.

--- src/main/java/com/github/fjtorres/deckOfCards/PokerSuit.java
+++ src/main/java/com/github/fjtorres/deckOfCards/PokerSuit.java
@@ -4,5 +4,15 @@ package com.github.fjtorres.deckOfCards;
  * Enumeration with all the suits for poker.
  */
 public enum PokerSuit {
-    HEARTS, SPADES, CLUBS, DIAMONDS
+    HEARTS("\u2665"), SPADES("\u2664"), CLUBS("\u2663"), DIAMONDS("\u2662");
+
+    private final String symbol;
+
+    PokerSuit(String pSymbol) {
+        this.symbol = pSymbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
